57a83063ed094e7b071ce62d9ae766ed6f9e7dfe
Martin Durant <martin.durant@alumni.utoronto.ca>
2025-07-23 16:15:24 -0400

    work

--- fsspec-proxy/fsspec_proxy/ws_server.py
+++ fsspec-proxy/fsspec_proxy/ws_server.py
@@ -1,7 +1,25 @@
+from contextlib import asynccontextmanager
+import json
+
 from fastapi import FastAPI, WebSocket
+import fsspec
 import starlette.websockets
 
+from fsspec_proxy import file_manager
+
 app = FastAPI()
+m = fsspec.filesystem("memory")
+m.pipe_file("mytests/afile", b"hello world")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # start instances in async context
+    app.manager = file_manager.FileSystemManager()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -9,38 +27,105 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         try:
             data = await websocket.receive_bytes()
-            uid = int.from_bytes(data[:4], "little")
-            print(uid)
-            await websocket.send_bytes(data)
+            meta_length = int.from_bytes(data[:2], byteorder="big")
+            meta = json.loads(data[2:2 + meta_length].decode())
+            print(meta)
+            uid = meta.pop("uid")
+            result = await dispatch(**meta)
+            if isinstance(result, dict):
+                result["uid"] = uid
+                await _send_bytes(websocket, result)
+            else:
+                await _send_bytes(websocket, {"uid": uid}, data=result)
         except starlette.websockets.WebSocketDisconnect:
             break
+        except Exception as e:
+            await _send_bytes(
+                websocket,
+              {"status": "error", "uid": uid, "error": str(e)}
+            )
 
 
-# example use in cpython
-from websockets.asyncio.client import connect
+async def _send_bytes(ws, message, data=None):
+    meta = json.dumps(message).encode()
+    payload = len(meta).to_bytes(2, "little") + meta
+    if data:
+        payload += data
+    await ws.send_bytes(payload)
 
-async def run_one():
-    async with connect("ws://localhost:8000/ws") as ws:
-        await ws.send(b"test")
-        print(await ws.recv())
 
-"""
-# example use in pyscript
-from pyscript import WebSocket
+routes = {}
 
-def onopen(event):
-    print(event.type)
-    ws.send("hello")
 
-def onmessage(event):
-    print(event.type, event.data)
-    ws.close()
+def register_route(route: str):
+    def wrapper(func):
+        routes[route] = func
+        return func
+    return wrapper
 
-def onclose(event):
-    print(event.type)
 
-ws = WebSocket(url="ws://localhost:8000/ws")
-ws.onopen = onopen
-ws.onmessage = onmessage
-ws.onclose = onclose
-"""
+async def dispatch(route, **op):
+    print(route, op)
+    return await routes[route](**op)
+
+
+@register_route("list")
+async def list_dir(key=None, path=""):
+    if not key:
+        keys = list(app.manager.filesystems)
+        return {
+            "status": "ok",
+            "contents": [
+                {"name": k, "size": 0, "type": "directory"} for k in keys
+            ]
+        }
+
+    fs_info = app.manager.get_filesystem(key)
+    if fs_info is None:
+        raise FileNotFoundError(key)
+    path = f"{fs_info['path'].rstrip('/')}/{path.lstrip('/')}"
+    out = await fs_info["instance"]._ls(path, detail=True)
+    out = [
+        {"name": f"{key}/{o['name'].replace(fs_info['path'], '', 1).lstrip('/')}",
+         "size": o["size"], "type": o["type"]}
+        for o in out
+    ]
+    return {"status": "ok", "contents": out}
+
+
+@app.delete("delete")
+async def delete_file(key, path):
+    fs_info = app.manager.get_filesystem(key)
+    path = f"{fs_info['path'].rstrip('/')}/{path.lstrip('/')}"
+    if fs_info is None:
+        raise FileNotFoundError
+    if fs_info.get("readonly"):
+        raise PermissionError
+    await fs_info["instance"]._rm_file(path)
+
+
+@register_route("get")
+async def get_bytes(key, path, start=None, end=None):
+    fs_info = app.manager.get_filesystem(key)
+    if fs_info is None:
+        raise FileNotFoundError
+    path = f"{fs_info['path'].rstrip('/')}/{path.lstrip('/')}"
+    return await fs_info["instance"]._cat_file(path, start=start, end=end)
+
+
+@app.post("put")
+async def put_bytes(key, path, data=b""):
+    fs_info = app.manager.get_filesystem(key)
+    if fs_info is None:
+        raise FileNotFoundError
+    if fs_info.get("readonly"):
+        raise PermissionError
+    path = f"{fs_info['path'].rstrip('/')}/{path.lstrip('/')}"
+    await fs_info["instance"]._pipe_file(path, data)
+    return {"status": "ok"}
+
+
+@register_route("health")
+async def ok():
+    return {"status": "ok"}
+
